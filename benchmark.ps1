5e5a1089ab4bbb7a4fe151371f26b52079daeece
Tomi Lamsa <lamsatom@gmail.com>
2016-08-11 14:35:58 +0300

    updated record script to make a file with time stamps, upload that file to github

--- benchmark.ps1
+++ benchmark.ps1
@@ -13,25 +13,39 @@ function Benchmark-Command ([ScriptBlock]$Expression, [int]$Samples = 1) {
 .EXAMPLE
   Benchmark-Command { ping -n 1 google.com } 3
 #>
-  #$timings = @()
+  $test_name = "uiautomator_amaze"
+  $file_path = "c:\users\Tomi\testAutomation\measurements\"
   [int]$Run = 1
+  $Start_time = Get-Date
+  #$filename = "$($test_name)$($start_time)"
+  $filename = "$test_name-$(get-date -f yyy-MM-dd_hh_mm_ss).txt"
+  echo $filename
+  $full_file_path = "$file_path$filename"
+  echo $full_file_path
+  #$full_file_path = "$file_path$test_name-$(get-date -f yyy-MM-dd_hh_mm_ss).txt"
+  #NUL > filename
+  #echo $filename
+  #echo $full_file_path
+  New-Item $full_file_path
+  
   do {
     $sw = New-Object Diagnostics.Stopwatch
     $sw.Start()
     $printout = & $Expression 2>&1
     $sw.Stop()
-    "run number: $Run" | Out-File c:\users\Tomi\testAutomation\measurements\robotium_amaze.txt -Append
-    "run time:  $($sw.Elapsed.TotalSeconds)" | Out-File c:\users\Tomi\testAutomation\measurements\robotium_amaze.txt -Append
-	"Command printout: " | Out-File c:\users\Tomi\testAutomation\measurements\robotium_amaze.txt -Append
-    $($printout) | Out-File c:\users\Tomi\testAutomation\measurements\robotium_amaze.txt -Append
-	"`n######################################################`n" | Out-File c:\users\Tomi\testAutomation\measurements\robotium_amaze.txt -Append 
+    "run number: $Run" | Out-File "$($full_file_path)" -Append
+    "run time:  $($sw.Elapsed.TotalSeconds)" | Out-File "$($full_file_path)" -Append
+	"Command printout: " | Out-File "$($full_file_path)" -Append
+    $($printout) | Out-File "$($full_file_path)" -Append
+	"`n######################################################`n" | Out-File "$($full_file_path)" -Append 
     $sw.Reset()
     $Samples--
     $Run++
   }
   while ($Samples -gt 0)
-  git add robotium_amaze.txt
-  git commit -m "results from START_TIME to END_TIME"
+  $End_time = Get-Date
+  git add $filename
+  git commit -m "results from $Start_time to $End_time"
   git push
 }
 
