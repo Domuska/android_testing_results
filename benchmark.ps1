08a49073258430791dec1700ce4ef111fea3c72c
Tomi Lamsa <lamsatom@gmail.com>
2016-08-12 10:04:38 +0300

    updated test recording script, now writes results to a csv and human-readable text file in a folder

--- benchmark.ps1
+++ benchmark.ps1
@@ -8,43 +8,61 @@ function Benchmark-Command ([ScriptBlock]$Expression, [int]$Samples = 1) {
   Hat tip to StackOverflow. http://stackoverflow.com/questions/3513650-a-commands-execution-in-powershell
   
   Remember to do dotexe stuff before running the test script:
-  . "path_to_this_script"
+  . "C:\Users\Tomi\testAutomation\measurements\benchmark.ps1"
+  
   
 .EXAMPLE
   Benchmark-Command { ping -n 1 google.com } 3
 #>
+
+
   #change file tag name below, it is added to beginning of file name
   $test_name = "uiautomator_amaze"
+  
+  
+  
   $file_path = "c:\users\Tomi\testAutomation\measurements\"
   [int]$Run = 1
-  $Start_time = Get-Date
-  $filename = "$test_name-$(get-date -f yyy-MM-dd_hh_mm_ss).txt"
+  $Start_time = Get-Date -f yyy-MM-dd_hh_mm_ss
+  #the raw file path, no extension
+  $filename = "$test_name-$(get-date -f yyy-MM-dd_hh_mm_ss)"
+  #$filename_txt = "$test_name-$(get-date -f yyy-MM-dd_hh_mm_ss).txt"
+  #$filename_csv = "$test_name-$(get-date -f yyy-MM-dd_hh_mm_ss).csv"
   #echo $filename
-  $full_file_path = "$file_path$filename"
-  #echo $full_file_path
-  New-Item $full_file_path
+  #in current directory
+  $full_file_path_txt = "$file_path$filename\$filename.txt"
+  $full_file_path_csv = "$file_path$filename\$filename.csv"
+  #echo $full_file_path_txt
+  
+  #create a new directory with the name 
+  New-Item "$file_path$filename" -type directory
   
   do {
     $sw = New-Object Diagnostics.Stopwatch
     $sw.Start()
     $printout = & $Expression 2>&1
     $sw.Stop()
-    "run number: $Run" | Out-File "$($full_file_path)" -Append
-    "run time:  $($sw.Elapsed.TotalSeconds)" | Out-File "$($full_file_path)" -Append
-	"Command printout: " | Out-File "$($full_file_path)" -Append
-    $($printout) | Out-File "$($full_file_path)" -Append
+	#write to human-readable .txt
+    "run number: $Run" | Out-File "$($full_file_path_txt)" -Append
+    "run time:  $($sw.Elapsed.TotalSeconds)" | Out-File "$($full_file_path_txt)" -Append
+	"Command printout: " | Out-File "$($full_file_path_txt)" -Append
+    $($printout) | Out-File "$($full_file_path_txt)" -Append
+	"`n######################################################`n" | Out-File "$($full_file_path_txt)" -Append 
+	#write to .txt that has tab separators between fields
+	"$($Run);$($sw.Elapsed.TotalSeconds);$($printout)" | Out-File "$($full_file_path_csv)" -Append -Encoding ascii
+	#"," | Out-File "$($full_file_path_csv)" -Append
+	#$sw.Elapsed.TotalSeconds | Out-File "$($full_file_path_csv)" -Append
 	
-	"`n######################################################`n" | Out-File "$($full_file_path)" -Append 
     $sw.Reset()
     $Samples--
     $Run++
   }
   while ($Samples -gt 0)
-  $End_time = Get-Date
+  $End_time = Get-Date -f yyy-MM-dd_hh_mm_ss
   #add the file to git, push with comment
   git add $filename
   git commit -m "results from $test_name - $Start_time to $End_time"
   git push
 }
 
-Export-ModuleMember Benchmark-Command
+#Export-ModuleMember Benchmark-Command
